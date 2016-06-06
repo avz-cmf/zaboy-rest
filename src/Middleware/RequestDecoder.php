0518f11266e77bb707227c350130eae0d40b067a
Net <Net@Boss>
2016-06-06 18:30:48 +0300

    Revert "1.0.0"
    This reverts commit 79a995ed377500f48d0dcaf1c017adeada0a6143.

--- src/Middleware/RequestDecoder.php
+++ src/Middleware/RequestDecoder.php
@@ -11,9 +11,17 @@ namespace zaboy\rest\Middleware;
 
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Xiag\Rql\Parser\ExpressionParser;
+use Xiag\Rql\Parser\Parser;
+use Xiag\Rql\Parser\Lexer;
+use Xiag\Rql\Parser\TokenParser\Query;
+use Xiag\Rql\Parser\TokenParserGroup;
+use Xiag\Rql\Parser\TokenParser;
+use Xiag\Rql\Parser\TypeCaster;
 use zaboy\rest\RestException;
+use zaboy\rest\RqlParser\SelectTokenParser;
 use Zend\Stratigility\MiddlewareInterface;
-use Xiag\Rql\Parser;
+
 
 /**
  * Parse body fron JSON and add result array to $request->withParsedBody()
@@ -32,6 +40,9 @@ use Xiag\Rql\Parser;
 class RequestDecoder implements MiddlewareInterface
 {
 
+    private $allowedAggregateFunction = ['count', 'sum', 'avg', 'min', 'max'];
+
+
     /**                         Location: http://www.example.com/users/4/
      *
      * @todo positionHeaders = 'beforeId'  'Put-Default-Position'  'Put-Default-Position'
@@ -46,6 +57,7 @@ class RequestDecoder implements MiddlewareInterface
         $overwriteModeHeader = $request->getHeader('If-Match');
         $overwriteMode = isset($overwriteModeHeader[0]) && $overwriteModeHeader[0] === '*' ? true : false;
         $request = $request->withAttribute('Overwrite-Mode', $overwriteMode);
+
         $putDefaultPosition = $request->getHeader('Put-Default-Position'); //'start' : 'end'
         if (isset($putDefaultPosition)) {
             $request = $request->withAttribute('Put-Default-Position', $putDefaultPosition);
@@ -54,10 +66,14 @@ class RequestDecoder implements MiddlewareInterface
         $putBeforeHeader = $request->getHeader('Put-Before');
         $putBefore = !empty($putBeforeHeader);
         $request = $request->withAttribute('Put-Before', $putBefore);
+
         $rqlQueryStringWithXdebug = $request->getUri()->getQuery();
+        
         $rqlQueryString = rtrim($rqlQueryStringWithXdebug, '&XDEBUG_SESSION_START=netbeans-xdebug');
         $rqlQueryObject = $this->parsRql($rqlQueryString);
         $request = $request->withAttribute('Rql-Query-Object', $rqlQueryObject);
+
+        
         $contenttype = $request->getHeader('Content-Type');
         if (isset($contenttype[0]) && false !== strpos($contenttype[0], 'json')) {
             $body = $this->jsonDecode($request->getBody()->__toString());
@@ -83,6 +99,7 @@ class RequestDecoder implements MiddlewareInterface
             );
         }
         json_encode(null);
+
         return $result;
     }
 
@@ -93,11 +110,55 @@ class RequestDecoder implements MiddlewareInterface
      */
     public function parsRql($rqlQueryString)
     {
-        $lexer = new Parser\Lexer();
+        /*
         $parser = Parser\Parser::createDefault();
         $tokens = $lexer->tokenize($rqlQueryString);
-        /* @var $rqlQueryObject \Xiag\Rql\Parser\Query */
-        $rqlQueryObject = $parser->parse($tokens);
+        * @var $rqlQueryObject \Xiag\Rql\Parser\Query *
+        $rqlQueryObject = $parser->parse($tokens);*/
+
+        $queryTokenParser = new TokenParserGroup();
+        $queryTokenParser
+            ->addTokenParser(new Query\GroupTokenParser($queryTokenParser))
+            ->addTokenParser(new Query\Basic\LogicOperator\AndTokenParser($queryTokenParser))
+            ->addTokenParser(new Query\Basic\LogicOperator\OrTokenParser($queryTokenParser))
+            ->addTokenParser(new Query\Basic\LogicOperator\NotTokenParser($queryTokenParser))
+
+            ->addTokenParser(new Query\Basic\ArrayOperator\InTokenParser())
+            ->addTokenParser(new Query\Basic\ArrayOperator\OutTokenParser())
+
+            ->addTokenParser(new Query\Basic\ScalarOperator\EqTokenParser())
+            ->addTokenParser(new Query\Basic\ScalarOperator\NeTokenParser())
+            ->addTokenParser(new Query\Basic\ScalarOperator\LtTokenParser())
+            ->addTokenParser(new Query\Basic\ScalarOperator\GtTokenParser())
+            ->addTokenParser(new Query\Basic\ScalarOperator\LeTokenParser())
+            ->addTokenParser(new Query\Basic\ScalarOperator\GeTokenParser())
+            ->addTokenParser(new Query\Basic\ScalarOperator\LikeTokenParser())
+
+            ->addTokenParser(new Query\Fiql\ArrayOperator\InTokenParser())
+            ->addTokenParser(new Query\Fiql\ArrayOperator\OutTokenParser())
+
+            ->addTokenParser(new Query\Fiql\ScalarOperator\EqTokenParser())
+            ->addTokenParser(new Query\Fiql\ScalarOperator\NeTokenParser())
+            ->addTokenParser(new Query\Fiql\ScalarOperator\LtTokenParser())
+            ->addTokenParser(new Query\Fiql\ScalarOperator\GtTokenParser())
+            ->addTokenParser(new Query\Fiql\ScalarOperator\LeTokenParser())
+            ->addTokenParser(new Query\Fiql\ScalarOperator\GeTokenParser())
+            ->addTokenParser(new Query\Fiql\ScalarOperator\LikeTokenParser());
+
+
+        $parser = (new Parser( (new ExpressionParser())
+            ->registerTypeCaster('string', new TypeCaster\StringTypeCaster())
+            ->registerTypeCaster('integer', new TypeCaster\IntegerTypeCaster())
+            ->registerTypeCaster('float', new TypeCaster\FloatTypeCaster())
+            ->registerTypeCaster('boolean', new TypeCaster\BooleanTypeCaster())
+        ))
+            ->addTokenParser(new SelectTokenParser($this->allowedAggregateFunction))
+            ->addTokenParser($queryTokenParser)
+            ->addTokenParser(new TokenParser\SortTokenParser())
+            ->addTokenParser(new TokenParser\LimitTokenParser());
+
+        $rqlQueryObject = $parser->parse((new Lexer())->tokenize($rqlQueryString));
+
         return $rqlQueryObject;
     }
 
