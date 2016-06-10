c5795932ef63485a97b6a99a52cd7978fa8f933b
Net <Net@Boss>
2016-06-10 23:06:43 +0300

    queue Broker

---
+++ src/FactoryAbstract.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Zaboy lib (http://zaboy.org/lib/)
+ *
+ * @copyright  Zaboychenko Andrey
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ */
+
+namespace zaboy\rest;
+
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+abstract class FactoryAbstract implements FactoryInterface
+{
+
+    /**
+     * Alias for "createService"
+     *
+     * @param ServiceLocatorInterface $serviceLocator
+     * @return mixed
+     */
+    abstract public function __invoke(ServiceLocatorInterface $serviceLocator);
+
+    /**
+     * {@inherit}
+     *
+     * {@inherit}
+     */
+    public function createService(ServiceLocatorInterface $serviceLocator)
+    {
+        return $this->__invoke($serviceLocator);
+    }
+
+}
